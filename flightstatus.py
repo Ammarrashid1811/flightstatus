875618b0d6bb55f6e571f4d78b787eff06994f89
Ammarrashid1811 <161561164+Ammarrashid1811@users.noreply.github.com>
2024-02-29 15:50:52 +0800

    Add files via upload

---
+++ flightstatus.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+st.write("""
+# Sales Prediction App
+
+This app predicts the **Flight status** 
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    CarrierCode = st.sidebar.number_input('CarrierCode', 0.0, 5.0, 2.0)
+    DepartureStation = st.sidebar.number_input('DepartureStation', 0.0, 109.0, 55.0)
+    Arrivalstation = st.sidebar.number_input('Newspaper', 0.0, 125.0, 70.0)
+    AOCHolder = st.sidebar.number_input('AOCHolder', 0.0, 7.0, 3.0)
+    data = {'CarrierCode': CarrierCode,
+            'DepartureStation': DepartureStation,
+            'Arrivalstation': Arrivalstation,
+            'AOCHolder'}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+loaded_model = pickle.load(open("flightdatac.h5", "rb"))
+
+prediction = loaded_model.predict(df)
+
+st.subheader('Prediction')
+st.write(prediction)
