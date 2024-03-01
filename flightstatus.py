ed1ee693be33be167b9856d79e6b86613afe10f6
Ammarrashid1811 <161561164+Ammarrashid1811@users.noreply.github.com>
2024-03-01 14:41:00 +0800

    Update flightstatus.py

--- flightstatus.py
+++ flightstatus.py
@@ -20,13 +20,13 @@ def user_input_features():
     DelayCode = st.sidebar.number_input('DelayCode', value=0)
     DelayTime = st.sidebar.number_input('DelayTime', value=0)
     data = {'CarrierCode': CarrierCode,
+            'FlightNumber': FlightNumber,
             'DepartureStation': DepartureStation,
             'Arrivalstation': Arrivalstation,
             'PAX': PAX,
             'AOCHolder': AOCHolder,
             'DelayCode': DelayCode,
-            'DelayTime': DelayTime
-           'FlightNumber': FlightNumber}
+            'DelayTime': DelayTime}
     features = pd.DataFrame(data, index=[0])
     return features
 
